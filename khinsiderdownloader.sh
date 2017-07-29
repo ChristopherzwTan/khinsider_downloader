836740160980b884022f4611af1d3f4da62a7cb9
ChristopherzwTan <christopher.tanzw@gmail.com>
2017-07-28 23:12:11 -0700

    Using variable instead

--- khinsiderdownloader.sh
+++ khinsiderdownloader.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+main_url=https://downloads.khinsider.com/game-soundtracks/album/ryu-ga-gotoku-zero-yakuza-0-ost-a-and-b-sides-
+
 # First get the initial webpage that holds all the links
-wget https://downloads.khinsider.com/game-soundtracks/album/ryu-ga-gotoku-zero-yakuza-0-ost-a-and-b-sides-
+wget $main_url
 
 # Grep for each mp3 link, these mp3 pages have the real mp3 link
-grep --color "https://downloads.khinsider.com/game-soundtracks/album/ryu-ga-gotoku-zero-yakuza-0-ost-a-and-b-sides-" * | cut -d '"' -f2 | uniq > links.txt
+grep --color "$main_url" * | cut -d '"' -f2 | uniq > links.txt
 
 # Download each of these mp3 pages that hold the real mp3 links
 wget -i links.txt
